49688eb069ffe1ba2a36b4cda66c337b7bc23fa6
Murillo Santos <murillo.lds01@gmail.com>
2025-11-09 17:24:37 -0300

    :sparkles: feat(react-app): implementa componente de categorias e página de categoria

---
+++ react-app/src/pages/CategoryPage.jsx
@@ -0,0 +1,39 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import Header from "../components/Header";
+import Categories from "../components/Categories";
+import ProductCard from "../components/ProductCard";
+import productService from "../services/productService";
+import './../components/Home.css';
+
+function CategoryPage() {
+    const { catName } = useParams();
+    const [products, setProducts] = useState([]);
+    
+    useEffect(() => {
+        productService.getProducts(catName)
+            .then((res) => {
+                setProducts(res.data.products);
+            })
+            .catch((err) => alert(err.response?.data?.message || 'Erro no servidor.'));
+    }, [catName]);
+    
+    return (
+        <div>
+            <Header />
+            <Categories />
+            <h5 style={{ marginTop: '15px', paddingLeft: '20px' }}> Mostrando produtos da categoria: {catName} </h5>
+            <div className="d-flex justify-content-center flex-wrap">
+                {products.length > 0 ? (
+                    products.map((item) => (
+                        <ProductCard key={item._id} product={item} />
+                    ))
+                ) : (
+                    <p>Nenhum produto encontrado nesta categoria.</p>
+                )}
+            </div>
+        </div>
+    );
+}
+
+export default CategoryPage;
