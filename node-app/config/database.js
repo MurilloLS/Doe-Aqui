e7ef95e9853efd82bd77ca28c5a51a63e09b9aa8
Murillo Santos <murillo.lds01@gmail.com>
2025-11-15 19:22:44 -0300

    :sparkles: feat(config): Adiciona .env, SDK da AWS e ajusta config da API

--- node-app/config/database.js
+++ node-app/config/database.js
@@ -2,8 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb+srv://<username>:<password>@doeaqui-cluster.dlc34az.mongodb.net/?retryWrites=true&w=majority&appName=doeaqui-cluster');
-
++       await mongoose.connect(process.env.MONGODB_URI);
         console.log('MongoDB conectado com sucesso.');
     } catch (err) {
         console.error('Falha ao conectar com o MongoDB:', err.message);
